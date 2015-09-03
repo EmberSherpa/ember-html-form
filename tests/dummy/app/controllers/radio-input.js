f4d0349dea121ccdf8cbfe62da4b162494ff4d92
Taras Mankovski <tarasm@gmail.com>
2015-09-03 13:06:52 -0400

    Made documentation pretty

--- tests/dummy/app/controllers/radio-input.js
+++ tests/dummy/app/controllers/radio-input.js
@@ -3,15 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['activeUnit'],
   activeUnit: 'cm',
-
   units: [
     { id: 'cm', name: 'Centimeters' },
     { id: 'in', name:'Inches' }
-  ],
-
-  actions: {
-    setActiveUnit(unit){
-      this.set('activeUnit', unit);
-    }
-  }
+  ]
 });
